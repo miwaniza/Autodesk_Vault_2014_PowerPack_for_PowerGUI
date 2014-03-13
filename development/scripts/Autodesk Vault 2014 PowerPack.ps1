d19d2e6bb5a6372afc4a8244f995214145ae79f8
miwaniza <miwaniza@gmail.com>
2014-03-13 17:02:45 +0200

    2.0.0.3
    The big deal of scripts exporting automated!

--- development/scripts/Autodesk Vault 2014 PowerPack.ps1
+++
@@ -1,4 +0,0 @@
-﻿if ($g_login -eq $null)
-	{Add-Type -Path "c:\Program Files (x86)\Autodesk\Autodesk Vault 2014 SDK\bin\Autodesk.DataManagement.Client.Framework.Vault.Forms.dll"
-	$global:g_login=[Autodesk.DataManagement.Client.Framework.Vault.Forms.Library]::Login($null)
-	}
